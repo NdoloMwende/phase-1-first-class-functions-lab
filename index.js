eb4f56b270286b676d40111563f18fe04f7c11e9
Ndolo Mercy <mercymwendend444@gmail.com>
2025-07-25 10:44:58 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers= arr => {
+    return arr.slice(0,2);
+}
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers= arr => {
+    return arr.slice(-2);
+}
+console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = (int) => (fare) => int * fare;
+ 
+const fareDoubler =createFareMultiplier(2);
+console.log (fareDoubler(10));
+
+const fareTripler=createFareMultiplier(3);
+console.log (fareTripler(10));
+
+function selectDifferentDrivers(arr,func){
+    return func(arr);
+}
+console.log(selectDifferentDrivers(drivers,returnFirstTwoDrivers));
+
+
